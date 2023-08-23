66a76b2f088c818e4256ecea398c3f2180e06369
tkkawa <tkm.kawa@gmail.com>
2023-08-23 21:29:17 +0900

    Add the feature to create container directory

--- src/create.rs
+++ src/create.rs
@@ -1,15 +1,28 @@
 use std::path::{Path, PathBuf};
+use std::fs;
 
 use anyhow::{bail, Ok, Result};
 use clap::Args;
 
 #[derive(Debug, Args)]
 pub struct Create {
-
+    #[clap(short, long)]
+    pid_file: Option<String>,
+    #[clap(short, long, default_value = ".")]
+    bundle: PathBuf,
+    #[clap(short, long)]
+    console_socket: Option<String>,
+    pub container_id: String,
 }
 
 impl Create {
     pub fn exec(&self, root_path: PathBuf) -> Result<()> {
+        let container_dir = root_path.join(&self.container_id);
+        if !container_dir.exists() {
+            fs::create_dir(&container_dir).unwrap();
+        } else {
+            bail!("{} already exists", self.container_id)
+        }
         Ok(())
     }
 }
