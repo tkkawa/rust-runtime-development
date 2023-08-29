54ff0728ff1328665867791ebbf3c032bca68456
tkkawa <tkm.kawa@gmail.com>
2023-08-29 21:43:04 +0900

    Add the state of container

--- src/container/container.rs
+++ src/container/container.rs
@@ -1,8 +1,9 @@
+use std::fs;
 use std::path::PathBuf;
 
 use anyhow::Result;
 
-use crate::container::ContainerStatus;
+use crate::container::{ContainerStatus, State};
 
 #[derive(Debug, Clone)]
 pub struct Container {
@@ -17,6 +18,8 @@ impl Container {
         bundle: &str,
         container_root: &PathBuf,
     ) -> Result<()> {
+        let container_root = fs::canonicalize(container_root)?;
+        let state = State::new(container_id, status, pid, bundle);
         Ok(())
     }
 }
--- src/container/state.rs
+++ src/container/state.rs
@@ -1,6 +1,23 @@
 use anyhow::{Ok, Result};
 use serde::{Deserialize, Serialize};
 
+pub struct State {
+
+}
+
+impl State {
+    pub fn new(
+        container_id: &str,
+        status: ContainerStatus,
+        pid: Option<i32>,
+        bundle: &str,
+    ) -> Self {
+        Self {
+            
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug, Copy, Clone, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub enum ContainerStatus {
