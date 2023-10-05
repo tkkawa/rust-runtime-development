ca9185ad54a529295314953d09aa4a42fce60d84
tkkawa <tkm.kawa@gmail.com>
2023-10-05 21:51:52 +0900

    Add prepare rootfs function

--- src/rootfs.rs
+++ src/rootfs.rs
@@ -17,6 +17,60 @@ use nix::NixPath;
 
 use crate::spec::{LinuxDevice, LinuxDeviceType, Mount, Spec};
 
+pub async fn prepare_rootfs(spec: &Spec, rootfs: &PathBuf, bind_devices: bool) -> Result<()> {
+    let mut flags = MsFlags::MS_REC;
+    match spec.linux {
+        Some(ref linux) => match linux.rootfs_propagation.as_ref() {
+            "shared" => flags |= MsFlags::MS_SHARED,
+            "private" => flags |= MsFlags::MS_PRIVATE,
+            "slave" | "" => flags |= MsFlags::MS_SLAVE,
+            _ => panic!(),
+        },
+        None => flags |= MsFlags::MS_SLAVE,
+    };
+    let linux = spec.linux.as_ref().unwrap();
+    mount(None::<&str>, "/", None::<&str>, flags, None::<&str>)?;
+
+    // log::debug!("mount root fs {:?}", rootfs);
+    mount(
+        Some(rootfs),
+        rootfs,
+        None::<&str>,
+        MsFlags::MS_BIND | MsFlags::MS_REC,
+        None::<&str>,
+    )?;
+
+    for m in &spec.mounts {
+        let (flags, data) = parse_mount(m);
+        // log::debug!("mount {:?}", m);
+        if m.typ == "cgroup" {
+            log::warn!("A feature of cgoup is unimplemented.");
+            // skip
+        } else if m.destination == PathBuf::from("/dev") {
+            mount_from(
+                m,
+                rootfs,
+                flags & !MsFlags::MS_RDONLY,
+                &data,
+                &linux.mount_label,
+            )?;
+        } else {
+            mount_from(m, rootfs, flags, &data, &linux.mount_label)?;
+        }
+    }
+
+    let olddir = getcwd()?;
+    chdir(rootfs)?;
+
+    setup_default_symlinks(rootfs)?;
+    create_devices(&linux.devices, bind_devices).await?;
+    setup_ptmx(rootfs)?;
+
+    chdir(&olddir)?;
+
+    Ok(())
+}
+
 pub fn pivot_rootfs<P: ?Sized + NixPath>(path: &P) -> Result<()> {
     let newroot = open(path, OFlag::O_DIRECTORY | OFlag::O_RDONLY, Mode::empty())?;
 
@@ -26,3 +80,4 @@ pub fn pivot_rootfs<P: ?Sized + NixPath>(path: &P) -> Result<()> {
     fchdir(newroot)?;
     Ok(())
 }
+
