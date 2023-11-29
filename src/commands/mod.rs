ed84b4bbc51dd7ce6e0b1c00a6656a0560576ab2
Takuma Kawamura <takuma@TakumanoMacBook-Pro.local>
2023-11-29 20:50:54 +0900

    Add empty construct_container_root method

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -6,6 +6,11 @@ use std::{
 
 pub mod delete;
 
+fn construct_container_root<P: AsRef<Path>>(root_path: P, container_id: &str) -> Result<PathBuf> {
+    Ok(())
+}
+
 fn container_exists<P: AsRef<Path>>(root_path: P, container_id: &str) -> Result<bool> {
+    let container_root = construct_container_root(root_path, container_id)?;
     Ok(())
 }
