05b388b779c47b3f7d161fbd26fe6a6700945abb
Takuma Kawamura <takuma@TakumanoMacBook-Pro.local>
2023-11-29 21:08:13 +0900

    Add the contents of load_container method

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -18,7 +18,13 @@ fn construct_container_root<P: AsRef<Path>>(root_path: P, container_id: &str) ->
 }
 
 fn load_container<P: AsRef<Path>>(root_path: P, container_id: &str) -> Result<Container> {
-    Ok(())
+    let container_root = construct_container_root(root_path, container_id)?;
+    if !container_root.exists() {
+        bail!("container {} does not exist.", container_id)
+    }
+
+    Container::load(container_root)
+        .with_context(|| format!("could not load state for container {contianer_id}"))
 }
 
 fn container_exists<P: AsRef<Path>>(root_path: P, container_id: &str) -> Result<bool> {
