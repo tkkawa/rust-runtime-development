554847c90bfca2b8b505ca971f818e26d2f1bf59
Takuma Kawamura <takuma@TakumanoMacBook-Pro.local>
2023-12-04 21:50:19 +0900

    Add empty kill_one_process and kill_all_processes methods

--- src/container/container_kill.rs
+++ src/container/container_kill.rs
@@ -8,6 +8,18 @@ impl Container {
         signal: S,
         all: bool,
     ) -> Result<(), LibcontainerError> {
+        if all {
+            self.kill_all_processes(signal)
+        } else {
+            self.kill_one_process(signal)
+        }
+    }
+
+    fn kill_one_process<S: Into<Signal>>(&self, signal: S) -> Result<(), LibcontainerError> {
+        
+    }
+
+    fn kill_all_processes<S: Into<Signal>>(&self, signal: S) -> Result<(), LibcontainerError> {
         
     }
 }
