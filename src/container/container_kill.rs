1c0cd089e5b7e979d0027b31e93095490960c48c
Takuma Kawamura <takuma@TakumanoMacBook-Pro.local>
2023-12-04 21:59:55 +0900

    Add the contents of kill_one_process method

--- src/container/container_kill.rs
+++ src/container/container_kill.rs
@@ -16,10 +16,23 @@ impl Container {
     }
 
     fn kill_one_process<S: Into<Signal>>(&self, signal: S) -> Result<(), LibcontainerError> {
-        
+        let signal = signal.into().into_raw();
+        let pid = self.pid().unwrap();
+
+        tracing::debug!("kill signal {} to {}", signal, pid);
+        match signal::kill(pid, signal) {
+            Ok(_) => {}
+            Err(nix::errno::Errno::ESRCH) => {
+                // the process does not exist, which is what we want
+            }
+            Err(err) => {
+                tracing::error!(id = ?self.id(), err = ?err, ?pid, ?signal, "failed to kill process");
+                return Err(LibcontainerError::OtherSyscall(err));
+            }
+        }
     }
 
     fn kill_all_processes<S: Into<Signal>>(&self, signal: S) -> Result<(), LibcontainerError> {
-        
+
     }
 }
